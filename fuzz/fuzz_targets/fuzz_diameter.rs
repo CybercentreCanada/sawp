8c02002560537b642092fb2e8b28f5beef875297
sa-sawp <sa-sawp@cyber.gc.ca>
2020-12-31 16:13:04 +0000

    Fuzz: Fix Diameter Parse Error

--- fuzz/fuzz_targets/fuzz_diameter.rs
+++ fuzz/fuzz_targets/fuzz_diameter.rs
@@ -2,11 +2,11 @@
 use libfuzzer_sys::fuzz_target;
 
 use sawp_diameter::Diameter;
-use sawp::parser::Parse;
+use sawp::parser::{Parse, Direction};
 
 fuzz_target!(|data: &[u8]| {
     let parser = Diameter {};
-    if let Err(e) = parser.parse(data) {
+    if let Err(e) = parser.parse(data, Direction::Unknown) {
         eprintln!("Diameter: Error parsing {:?}", e);
     }
 });
