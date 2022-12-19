4e2e2f5e673b7f8e88548e2570ae9fdbc7be40cd
William Correia <william.correia@cyber.gc.ca>
2022-12-19 15:05:57 -0500

    sawp: Add IP FFI
    - Support Ipv4Addr, Ipv6Addr, and IpAddr types. Provide is_X and getters
    for each type and raw data getters for v6 and v4
    - Manually add forward declarations of these types as cbindgen does not
    generate them.

--- src/ffi.rs
+++ src/ffi.rs
@@ -30,3 +30,57 @@ pub unsafe extern "C" fn sawp_string_get_ptr(s: *const String) -> *const u8 {
 pub unsafe extern "C" fn sawp_string_get_size(s: *const String) -> usize {
     deref!(s).len()
 }
+
+/// # Safety
+/// function will panic if called with null
+#[no_mangle]
+pub unsafe extern "C" fn sawp_ipv4addr_get_data(ip: *const std::net::Ipv4Addr) -> u32 {
+    u32::from_be_bytes(deref!(ip).octets())
+}
+
+/// # Safety
+/// function will panic if called with null
+#[no_mangle]
+pub unsafe extern "C" fn sawp_ipv6addr_get_data(ip: *const std::net::Ipv6Addr) -> *const u8 {
+    deref!(ip).octets().as_slice().as_ptr()
+}
+
+/// # Safety
+/// function will panic if called with null
+#[no_mangle]
+pub unsafe extern "C" fn sawp_ipaddr_is_v4(ip: *const std::net::IpAddr) -> bool {
+    deref!(ip).is_ipv4()
+}
+
+/// # Safety
+/// function will panic if called with null
+#[no_mangle]
+pub unsafe extern "C" fn sawp_ipaddr_is_v6(ip: *const std::net::IpAddr) -> bool {
+    deref!(ip).is_ipv6()
+}
+
+/// # Safety
+/// function will panic if called with null
+#[no_mangle]
+pub unsafe extern "C" fn sawp_ipaddr_as_v4(
+    ip: *const std::net::IpAddr,
+) -> *const std::net::Ipv4Addr {
+    if let std::net::IpAddr::V4(addr) = deref!(ip) {
+        addr
+    } else {
+        std::ptr::null()
+    }
+}
+
+/// # Safety
+/// function will panic if called with null
+#[no_mangle]
+pub unsafe extern "C" fn sawp_ipaddr_as_v6(
+    ip: *const std::net::IpAddr,
+) -> *const std::net::Ipv6Addr {
+    if let std::net::IpAddr::V6(addr) = deref!(ip) {
+        addr
+    } else {
+        std::ptr::null()
+    }
+}
